f22e50d8c7f83b5ba9aeebf0c5768ff255257226
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2022-10-10 12:18:57 +0700

    feat: create header

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import './scss/app.scss';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content"></div>
+    </div>
+  );
+}
+
+export default App;
