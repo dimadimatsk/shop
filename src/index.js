3bd3224e4545d8d4616e5a489762843c43291afa
Dmitriy Shilovskiy <dimareectsk@gmail.com>
2022-11-24 22:06:55 +0700

    fix: change basepath

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/shop">
+  <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
